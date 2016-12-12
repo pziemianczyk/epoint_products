f8bdbab122c8ebcaa4e14c1cbb4c9e76877895aa
Pawel Ziemianczyk <pziemianczyk@e-point.pl>
2016-12-12 15:13:31 +0100

    Praca z baza danych, wersja bez DataSource

---
+++ main/src/com/products/test/db/PostgresqlTest.java
@@ -0,0 +1,9 @@
+package com.products.test.db;
+
+/**
+ * Created by pziemianczyk on 12.12.16.
+ */
+public class PosgresqlTest {
+
+    
+}
---
+++ test/src/com/products/model/data/ProductDatabaseManagerImplTest.java
@@ -0,0 +1,7 @@
+package com.products.model.data;
+
+/**
+ * Created by pziemianczyk on 12.12.16.
+ */
+public class ProductDatabaseManagerImplTest {
+}
